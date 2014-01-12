26e5d71bd58671df795410d58db89ce214b57788
Andreas Heiberg <heibergandreas@gmail.com>
2014-01-12 17:58:54 +0100

    added .form-control to bootstrap textarea

--- src/views/themes/bootstrap/forms/textarea.blade.php
+++ src/views/themes/bootstrap/forms/textarea.blade.php
@@ -1,7 +1,7 @@
 <div class="control-group {{{ $class }}} {{ $errors->has($id) ? 'error' : false }}">
 	<label for="{{ $id }}" class="control-label">{{ $text }} {{ $required ? '<span class="required-red">*</span>' : ''}}</label>
 	<div class="controls">
-		<textarea name="{{ $id }}" id="{{ $id }}" cols="30" rows="10" {{ $disabled ? 'readonly' : '' }}>{{ $value }}</textarea>
+		<textarea class="form-control" name="{{ $id }}" id="{{ $id }}" cols="30" rows="10" {{ $disabled ? 'readonly' : '' }}>{{ $value }}</textarea>
 		@if ($helpText)
 			<span class='help-block'>{{ $helpText }}</span>
 		@endif
