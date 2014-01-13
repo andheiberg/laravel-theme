2db600fb6fee85eb6ef0efc0347bd86f6c5f8a52
Andreas Heiberg <heibergandreas@gmail.com>
2014-01-13 10:53:34 +0100

    added .form-control to data template

--- src/views/themes/bootstrap/forms/date.blade.php
+++ src/views/themes/bootstrap/forms/date.blade.php
@@ -1,7 +1,7 @@
 <div class="control-group {{ $errors->has($id) ? 'error' : false }}">
 	<label for="{{ $id }}" class="control-label">{{ $text }} {{ $required ? '<span class="required-red">*</span>' : ''}}</label>
 	<div class="controls">
-		<input type="date" id="{{ $id }}" name="{{ $id }}" value="{{ $value }}" {{ $disabled ? 'readonly' : '' }}>
+		<input type="date" class="form-control" id="{{ $id }}" name="{{ $id }}" value="{{ $value }}" {{ $disabled ? 'readonly' : '' }}>
 		@if ($helpText)
 			<span class='help-block'>{{ $helpText }}</span>
 		@endif
