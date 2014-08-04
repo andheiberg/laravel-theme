0c48943305e859dd22c505060f882d5ae1e3f22c
Andreas Heiberg <heibergandreas@gmail.com>
2014-08-04 15:48:50 -0700

    added placeholder to password

--- src/views/themes/bootstrap/forms/password.blade.php
+++ src/views/themes/bootstrap/forms/password.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group {{ $errors->has($id) ? 'has-error' : false }}">
 	<label for="{{ $id }}" class="control-label">{{ $text }} {{ $required ? '<span class="required-red">*</span>' : ''}}</label>
 	<div class="form-controls">
-		<input type="password" id="{{ $id }}" name="{{ $id }}" value="{{ $value }}" class="form-control" {{ $disabled ? 'readonly' : '' }}>
+		<input type="password" id="{{ $id }}" name="{{ $id }}" value="{{ $value }}" class="form-control" {{ $disabled ? 'readonly' : '' }} placeholder="{{ $placeholder }}">
 		@if ($helpText)
 			<span class='help-block'>{{ $helpText }}</span>
 		@endif
