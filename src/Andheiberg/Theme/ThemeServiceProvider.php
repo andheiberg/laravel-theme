2e786dfad7e7e9d43b4d9d9546413b4286ad202e
AndreasHeiberg <heibergandreas@gmail.com>
2015-06-07 03:39:49 -0700

    [bug fix] windows matching was causing problems

--- src/Andheiberg/Theme/ThemeServiceProvider.php
+++ src/Andheiberg/Theme/ThemeServiceProvider.php
@@ -76,7 +76,7 @@ class ThemeServiceProvider extends ServiceProvider {
 
 		$blade->extend(function($view, $compiler)
 		{
-			$matcher = '/\-@(.*)(?=\r\n)/';
+			$matcher = '/\-@(.*)(?=\r\n)?/';
 			
 			return preg_replace($matcher, '<?php echo Theme::$1End(); ?>', $view);
 		});
